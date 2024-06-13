8c1f795fa85f00f2a0d696ad432b172284306cc8
Matias Aleksandrowicz <matiasmaleks@gmail.com>
2024-06-13 14:39:08 -0300

    TDA Proga2

---
+++ src/main/java/org/example/implementation/dynamic/DynamicStack.java
@@ -0,0 +1,38 @@
+package org.example.implementation.dynamic;
+
+import org.example.definition.Stack;
+import org.example.implementation.dynamic.nodes.Node;
+
+import java.util.Objects;
+
+public class DynamicStack implements Stack {
+
+    private Node first;
+
+
+    @Override
+    public void add(int a) {
+        this.first = new Node(a, this.first);
+    }
+
+    @Override
+    public void remove() {
+        if(this.isEmpty()) {
+            throw new RuntimeException("No se puede desapilar una pila vacia");
+        }
+        this.first = this.first.getNext();
+    }
+
+    @Override
+    public int getTop() {
+        if(this.isEmpty()) {
+            throw new RuntimeException("No se puede desapilar una pila vacia");
+        }
+        return this.first.getValue();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return Objects.isNull(this.first);
+    }
+}
